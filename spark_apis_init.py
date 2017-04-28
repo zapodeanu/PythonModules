7abcac0d3fabefcfb16330bbf18c9c577bdd2a50
Gabriel Zapodeanu <gzapodea@cisco.com>
2017-04-28 13:16:10 -0700

    create the file
    This file will include the URL API resource and Spark Token

---
+++ spark_apis_init.py
@@ -0,0 +1,8 @@
+
+
+# developed by Gabi Zapodeanu, TSA, GSS, Cisco Systems
+
+# !/usr/bin/env python3
+
+SPARK_URL = 'https://api.ciscospark.com/v1'
+SPARK_AUTH = 'Bearer ' + 'Put Your Spark Token Here'
