064c0ba013fd8867f333b2a0ed5efc3fb0d3bbd7
Gabriel Zapodeanu <gzapodea@cisco.com>
2017-05-05 13:41:10 -0700

    Update and rename spark_apis_init.py to modules_init.py

---
+++ modules_init.py
@@ -0,0 +1,23 @@
+
+# developed by Gabi Zapodeanu, TSA, GSS, Cisco Systems
+
+# !/usr/bin/env python3
+
+
+# Spark init info
+
+SPARK_URL = 'https://api.ciscospark.com/v1'
+SPARK_AUTH = 'Bearer ' + 'Put Your Spark Token Here'
+
+
+# Meraki init info
+
+MERAKI_API_KEY = '6d5ca08abf9bcb0393513841950a3889aefd6577'  # meraki sandbox
+MERAKI_URL = 'https://dashboard.meraki.com/api/v0'
+
+
+# APIC-EM init info
+
+EM_URL = 'https://sandboxapic.cisco.com//api/v1'
+EM_USER = 'devnetuser'
+EM_PASSW = 'Cisco123!'
--- spark_apis_init.py
+++
@@ -1,7 +0,0 @@
-
-# developed by Gabi Zapodeanu, TSA, GSS, Cisco Systems
-
-# !/usr/bin/env python3
-
-SPARK_URL = 'https://api.ciscospark.com/v1'
-SPARK_AUTH = 'Bearer ' + 'Put Your Spark Token Here'
