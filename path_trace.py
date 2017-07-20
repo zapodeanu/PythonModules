c30377bc470b1cf9acd68111b2b267966b4f7ba1
gzapodea <gzapodea@cisco.com>
2017-07-20 10:48:49 -0700

    work in progress

--- path_trace.py
+++ path_trace.py
@@ -11,24 +11,27 @@ import time
 import requests.packages.urllib3
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
-import apic_em_apis
-import utils
-from modules_init import EM_USER, EM_PASSW
+import apic_em_apis  # import the APIC-EM module
+import utils  # import the utils module
+from modules_init import EM_USER, EM_PASSW  # import the APIC-EM username and password
 
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)  # Disable insecure https warnings
 
 
 def main():
     """
-    The script will initiate an APIC-EM path trace using the Cisco DevNet Sandbox: https://sandboxapicem.cisco.com/
-    APIC-EM url, username and password are configured in the modules_init module. Change these variables if needed to
-    test with a different controller.
+    The script will create an APIC-EM path trace using the Cisco DevNet Sandbox: https://sandboxapicem.cisco.com/.
+    We will retrieve the path trace details and ask the user if to delete the created path trace at the end.
+    APIC-EM url, username and password are configured in the modules_init module.
+    Change these variables if needed to test with a different controller.
     The code will use functions included with the apic_em_apis module.
+
     Source IP address: 10.1.15.117
     Destination IP address: 10.2.1.22
     """
 
-    # declarations for source and destination IP addresses
+    # declarations for source and destination IP addresses,
+    # change these variables if needed to test with different IP addresses
 
     src_ip = '10.1.15.117'
     dest_ip = '10.2.1.22'
