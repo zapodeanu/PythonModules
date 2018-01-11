c18697cc3dc4f4b5659097ee2013e5683e685f14
gzapodea <gzapodea@cisco.com>
2018-01-10 20:53:32 -0800

    work in progress

--- utils.py
+++ utils.py
@@ -1,5 +1,4 @@
 
-
 # developed by Gabi Zapodeanu, TSA, GPO, Cisco Systems
 
 # !/usr/bin/env python3
@@ -12,12 +11,12 @@ import select
 import requests
 import time
 import requests.packages.urllib3
-import PIL
 import os
 import os.path
+import socket  # needed for IPv4 validation
+import re  # needed for regular expressions matching
 
-
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image, ImageDraw, ImageFont  # needed for the image processing
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 from requests.auth import HTTPBasicAuth  # for Basic Auth
 
@@ -98,5 +97,47 @@ def image_annotate(in_image, out_image, text, color, font_size, x, y):
     draw = ImageDraw.Draw(image)  # edit image to annotate
     fonts_folder = '/Library/Fonts'  # for MAC OS X - folder with the fonts
     arial_font = ImageFont.truetype(os.path.join(fonts_folder, 'Arial Black.ttf'), font_size)  # select the font and size
-    draw.text((x,y), text, fill=color, font=arial_font)  # annotate with text
-    image.save(out_image,'PNG')  # save new image
+    draw.text((x, y), text, fill=color, font=arial_font)  # annotate with text
+    image.save(out_image, 'PNG')  # save new image
+
+
+def validate_ipv4_(ip_address):
+    """
+    This function will validate if the provided string is a valid IPv4 address
+    :param ip_address: string with the ip address
+    :return: true/false
+    """
+    try:
+        socket.inet_aton(ip_address)
+        return True
+    except:
+        return False
+
+
+def identify_ipv4_address(configuration):
+    """
+    This function will return a list of all IPv4 addresses found in the string {configuration}.
+    It will return only the IPv4 addresses found in the {ip address a.b.c.d command}
+    :param configuration: string with the configuration
+    :return: list of IPv4 addresses
+    """
+    ipv4_list =[]
+    pattern = re.compile('^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$')
+    split_lines = configuration.split('\n')  # split configuration file in individual commands'
+    for line in split_lines:
+        print(line)
+        if 'ip address' in line:  # check if command includes the string 'ip address'
+            split_config = line.split(' ')  # split the command in words
+            try:
+                split_config.remove('')  # remove the first ' ' if existing in the command
+            except:
+                pass
+            line_begins = split_config[0:3]  # select the three items in the list
+            for word in line_begins:
+                check_ip = pattern.match(word)  # match each word with the patern from regex
+                if check_ip:
+                    if validate_ipv4_address(word):  # validate if the octets are valid IP addresses
+                        ipv4_list.append(word)
+    return ipv4_list
+
+
